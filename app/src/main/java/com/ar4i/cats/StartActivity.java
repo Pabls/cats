ce894ce88b1d46c6f69ec842b483202fe95c6c1b
Pabls <pabls@yandex.ru>
2019-03-21 12:10:53 +0300

    init

---
+++ app/src/main/java/com/ar4i/cats/StartActivity.java
@@ -0,0 +1,13 @@
+package com.ar4i.cats;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
---
+++ app/src/main/java/com/ar4i/cats/app/CatsApp.java
@@ -0,0 +1,6 @@
+package com.ar4i.cats.app;
+
+import android.app.Application;
+
+public class CatsApp extends Application {
+}
